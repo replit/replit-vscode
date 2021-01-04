986995ff87a4ed4d38df7093a1dda519a1eceb53
masad-frost <farismasad@gmail.com>
2021-01-04 15:57:19 +0400

    replit.init works

--- src/fs.ts
+++ src/fs.ts
@@ -26,37 +26,66 @@ export class FS implements vscode.FileSystemProvider {
     this.onDidChangeFile = this.emitter.event;
   }
 
+  private uriToProtocolPath() {
+
+  }
+
   // What is this for?
   // async copy() {}
 
   watch(uri: vscode.Uri): vscode.Disposable {
     console.log('watch', uri.path);
+    // What is this for?
+    return new vscode.Disposable(() => { });
   }
-  
+
   async createDirectory(uri: vscode.Uri): Promise<void> {
     console.log('createDirectory', uri.path);
   }
 
   async readDirectory(uri: vscode.Uri): Promise<[string, vscode.FileType][]> {
     console.log('readDirectory', uri.path);
+
+    if (uri.path === '/') {
+      return [];
+    }
+
+    throw vscode.FileSystemError.FileNotFound();
   }
 
 
   async readFile(uri: vscode.Uri): Promise<Uint8Array> {
     console.log('readFile', uri.path);
+
+    throw vscode.FileSystemError.FileNotFound();
   }
 
   async delete(uri: vscode.Uri): Promise<void> {
     console.log('delete', uri.path);
+
+    throw vscode.FileSystemError.FileNotFound();
+
   }
 
 
   async rename(oldUri: vscode.Uri, newUri: vscode.Uri, options: { overwrite: boolean }): Promise<void> {
     console.log('rename', oldUri.path, newUri.path, options);
+
+    throw vscode.FileSystemError.FileNotFound();
   }
 
   async stat(uri: vscode.Uri): Promise<vscode.FileStat> {
     console.log('stat', uri.path);
+    if (uri.path === '/') {
+      return {
+        ctime: Date.now(),
+        mtime: Date.now(),
+        size: 0,
+        type: vscode.FileType.Directory
+      };
+    }
+
+    throw vscode.FileSystemError.FileNotFound();
   }
 
   async writeFile(uri: vscode.Uri, content: Uint8Array, options: { create: boolean, overwrite: boolean }): Promise<void> {
